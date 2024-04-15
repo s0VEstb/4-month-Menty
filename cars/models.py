9e9bf3a437769e1fab8b642abdc471f5945482b1
AidemirK <demirkarmil@gmail.com>
2024-04-15 19:07:32 +0600

    4-HW2

--- cars/models.py
+++ cars/models.py
@@ -1,3 +1,22 @@
+import datetime
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    img = models.ImageField(upload_to='media/',null=True, blank=True)
+    title = models.CharField(max_length=200, null=True)
+    brand = models.CharField(max_length=255, null=True)
+    model = models.CharField(max_length=255)
+    price = models.CharField(max_length=255)
+    year_of_issue = models.IntegerField(validators=[
+            MinValueValidator(1900),
+            MaxValueValidator(datetime.date.today().year)
+        ])
+    text = models.TextField(null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    
+    
+    def __str__(self):
+        return self.title
--- cars/views.py
+++ cars/views.py
@@ -2,15 +2,18 @@ from datetime import datetime
 import random
 from django.shortcuts import render, HttpResponse
 
-def main_view(request):
-    now = datetime.now()
-    
-    formatted_now = now.strftime("%Y-%m-%d %H:%M:%S")
+from cars.models import Post
 
-    context = {
-        'current_date_time': formatted_now
-    }
-    return render(request, 'main.html', context)
+def main_view(request):
+    if request.method == 'GET':
+        now = datetime.now()
+        
+        formatted_now = now.strftime("%Y-%m-%d %H:%M:%S")
+
+        context = {
+            'current_date_time': formatted_now
+        }
+        return render(request, 'main.html', context)
     
 
 
@@ -20,10 +23,25 @@ def hello_view(request):
 
 
 def fun_view(request):
-    anecdotes = [
-        "Если у вас закончилась мазь от зуда,\n — Не спешите выбрасывать тюбик.\n Его уголком очень удобно чесаться.",
-        "Бармен спрашивает пьяного посетителя:\n— Я вижу, у вас пустой стакан. Не хотите ли еще один?\nА на хрена мне два пустых стакана?",
-        "Дорогой, ты с чем картошечку будешь на ужин?\n— С мясом.\nЯ как знала и купила чипсы с беконом."
-    ]
-    anecdote = random.choice(anecdotes)
-    return HttpResponse(anecdote)
+    if request.method == 'GET':
+        anecdotes = [
+            "Если у вас закончилась мазь от зуда,\n — Не спешите выбрасывать тюбик.\n Его уголком очень удобно чесаться.",
+            "Бармен спрашивает пьяного посетителя:\n— Я вижу, у вас пустой стакан. Не хотите ли еще один?\nА на хрена мне два пустых стакана?",
+            "Дорогой, ты с чем картошечку будешь на ужин?\n— С мясом.\nЯ как знала и купила чипсы с беконом."
+        ]
+        anecdote = random.choice(anecdotes)
+        return HttpResponse(anecdote) 
+
+def post_list_view(request):
+    if request.method == 'GET':
+        posts = Post.objects.all()
+        context = {'posts': posts}
+        
+        return render (request, 'cars/post_list.html', context)
+    
+def post_detail_view(request, post_id):
+    if request.method == 'GET':
+        post = Post.objects.get(id=post_id)
+        context = {'post': post}
+        
+        return render (request, 'cars/post_detail.html', context)
